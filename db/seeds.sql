dff813dcbaed4fdddd268d88002887b82a469da0
apereira91 <52183556+apereira91@users.noreply.github.com>
2020-02-22 12:58:15 -0500

    Revert "Models"

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO watchList(movieTitle, watched)
+VALUES ("The Book of Eli", TRUE)
+
+INSERT INTO watchList(movieTitle, watched)
+VALUES ("Fight Club", TRUE)
+
+INSERT INTO watchList(movieTitle, watched)
+VALUES ("Star Wars", FALSE)
