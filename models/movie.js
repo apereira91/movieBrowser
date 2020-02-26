b00354097aab5d0bf62c07b59903b5641f097de8
sjmarsnc <sjmarsnc@gmail.com>
2020-02-25 23:18:22 -0500

    proper spacing

--- models/movie.js
+++ models/movie.js
@@ -1,17 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
-    var Movie = sequelize.define("Movie", {
-      title: {
-        type: DataTypes.STRING,
+  var Movie = sequelize.define("Movie", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    movieId: { type: DataTypes.STRING }
+  });
+  Movie.associate = function (models) {
+    Movie.belongsTo(models.User, {
+      foreignKey: {
         allowNull: false
-      },
-      movieId: { type: DataTypes.STRING }
+      }
     });
-    Movie.associate = function (models) {
-      Movie.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    }
-    return Movie;
-  };
+  }
+  return Movie;
+};
