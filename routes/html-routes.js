c0350f193814d0122d1694cf7dbefaa54a5d8d51
travis-john <travis.wl.john@gmail.com>
2020-02-25 21:27:45 -0500

    isAuthenticated logic

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.render("index", {isAuthenticated: true});
     }
     // res.sendFile(path.join(__dirname, "../public/signup.html"));
 
@@ -46,7 +46,7 @@ module.exports = function (app) {
   app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/", {isAuthenticated: true});
     }
     // res.sendFile(path.join(__dirname, "../public/login.html"));
     res.render("login");
@@ -54,9 +54,9 @@ module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function (req, res) {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
-    res.render("members");
-  });
+  // app.get("/members", isAuthenticated, function (req, res) {
+  //   // res.sendFile(path.join(__dirname, "../public/members.html"));
+  //   res.render("members");
+  // });
 
 };
