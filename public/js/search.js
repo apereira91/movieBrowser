c9f376381de48e4cb373beb084505065c25e5466
travis-john <travis.wl.john@gmail.com>
2020-02-27 21:12:28 -0500

    pushing for collaboration

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     // Getting references to our form and input
-    var searchForm = $(".search");
+    var searchForm = $(".api-search");
+    var submitBtn = $("#search-submit");
+    submitBtn.on("click", function(event) {
+        event.preventDefault();
+
+      }
+}
